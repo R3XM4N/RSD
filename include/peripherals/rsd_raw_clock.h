ae6ed6efe8e67d6fe22af59680661a4bfbe297b4
Rexxi <76392112+R3XM4N@users.noreply.github.com>
2026-08-26 02:53:02 +0200

    Ticker works! On pico2w

--- include/peripherals/rsd_raw_clock.h
+++ include/peripherals/rsd_raw_clock.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define PLL_SYS_BASE        0x40028000u
+#define PLL_SYS_BASE        0x40050000u
 #define PLL_SYS_CS          (*(volatile uint32_t*)(PLL_SYS_BASE + 0x00)) // Control/status
 #define PLL_SYS_FBDIV_INT   (*(volatile uint32_t*)(PLL_SYS_BASE + 0x08)) // Feedback divider
 #define PLL_SYS_PRIM        (*(volatile uint32_t*)(PLL_SYS_BASE + 0x0C)) // Post-dividers
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdint.h>
 
-// #include "include/interrupt/rsd_sys_tick.h"
+#include "include/interrupt/rsd_sys_tick.h"
 #include "include/peripherals/rsd_gpio.h"
-// #include "include/peripherals/rsd_raw_clock.h"
+#include "include/peripherals/rsd_raw_clock.h"
 // #include "include/interrupt/rsd_interrupt.h"
 
 // #include "include/os/rsd_os.h"
@@ -10,43 +10,31 @@ int32_t asm_add(int32_t a, int32_t b);
 
 
 int main(void){
-    // sys_tick_init(get_sys_clock_hz());
+    sys_tick_init(get_sys_clock_hz());
     // irq_init();
     // irq_register(13, isr_io_bank0);
 
     // (*(volatile uint32_t*)0xE000E200u) = (1u << 13);
 
     gpio_enable_out(16);
+    gpio_enable_out(17);
     if (asm_add(5, 5) > 9){
         gpio_write(16);
     }
-    if (asm_add(5, 5) < 15){
-        gpio_clear(16);
-    }
+    gpio_write(17);
+
+    // if (asm_add(5, 5) < 15){
+    //     gpio_clear(16);
+    // }
     // gpio_clear(16);
     
     // gpio_enable_fall_irq(15);
 
     // test_call();
     while (1){
-        // int32_t result = asm_add(3, 4);
-        // if (result > 5){
-        //     gpio_write(25);
-        // }
-        // delay_ms(500);
-        // gpio_write(16);
-
-        // result = asm_add(3, 1);
-        // if (result < 5){
-        //     gpio_clear(25);
-        // }
-        // // if (!gpio_read(18)){
-        // //     gpio_write(20);
-        // // }
-        // // else {
-        // //     gpio_clear(20);
-        // // }
-        
-        // delay_ms(500);
+        gpio_write(17);
+        delay_ms(500);
+        gpio_clear(17);
+        delay_ms(500);
     }
 }
